886e4b3fa67cf6636da00012bc96b3167b56c781
mayk0l <maykol0219@gmail.com>
2023-05-30 07:25:10 -0400

    anterior change didn't fixed the error, this one yes

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,15 +1,9 @@
-"use client"
+
 import '../../styles/global.css'
 import Navbar from "@/components/Navbar"
 import Providers from "@/components/ThemeProvider";
 import ThemeChanger from "@/components/ThemeChanger";
 import Footer from '@/components/Footer'
-import Head from 'next/head';
-
-export const metadata = {
-  title: "Portafolio",
-  description: "...",
-};
 
 export default function RootLayout({
   children,
@@ -18,17 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
-      </Head>
+      
       <body className="dark:bg-stone-900">
         <Providers>
           <ThemeChanger />
